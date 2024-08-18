8899ba2b09af5ef5abe30c0a2c3b16a4ed78c781
HRusby <harrygeorger@hotmail.com>
2024-08-18 15:27:00 +0000

    Migrating NVim to use AstroNvim

--- .config/nvim/lua/user/lsp/settings/bashls.lua
+++
@@ -1,8 +0,0 @@
-return {
-  cmd = { "bash-language-server", "start" },
-  cmd_env = {
-    GLOB_PATTERN = "*@(.sh|.inc|.bash|.command)"
-  },
-  filetypes = { "sh" },
-  single_file_support = true,
-}
--- .config/nvim/lua/user/lsp/settings/yamlls.lua
+++
@@ -1,9 +0,0 @@
-return {
-  settings = {
-    yaml = {
-      schemas = {
-        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "kubernetes/**/*.yaml"
-      },
-    },
-  },
-}
